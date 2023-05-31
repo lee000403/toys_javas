9b7e1b90a1c27602e245dd1d5e79251398a44f1a
YebinKim98 <dpqls9898@gmail.com>
2023-05-31 13:04:24 +0900

    0531

--- src/MaxMins.java
+++ src/MaxMins.java
@@ -10,6 +10,16 @@ public class MaxMins {
             end = first;
         } return end;
     }
+    public int Min(int first, int second) { // Min 함수
+        int end = 0;
+        if (first < second) {
+          end = first; 
+        } else if (first > second) {
+            end = second;
+        } else {
+            end = first;
+        } return end;
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in); // 입력 값 두개 받기
         System.out.print("숫자 2개 입력 : ");
@@ -18,5 +28,7 @@ public class MaxMins {
         MaxMins maxMins = new MaxMins(); // max 함수 호출
         int end = maxMins.Max(first, second);
         System.out.println("Max 값 : " + end);
+        int end_min = maxMins.Min(first, second);
+        System.out.println("Min 값 : " + end_min);
     }
 }
