4d56619bc1626eebd79ecb3426e5ec936f2c30fe
lee000403 <dong000403@gmail.com>
2023-06-13 17:39:58 +0900

    main 함수

--- src/PollsWithDBInTerminal/PollsWithDB.java
+++ src/PollsWithDBInTerminal/PollsWithDB.java
@@ -1,5 +1,54 @@
 package PollsWithDBInTerminal;
 
+import java.sql.*;
+import java.util.Scanner;
+
 public class PollsWithDB {
-    
+    public static void main(String[] args) {
+        try {
+            // sql (url, user, password 변수 저장)
+            String url = "jdbc:mysql://127.0.0.1:3306/db_cars";
+            String user = "root";
+            String password = "!yojulab*";
+
+            // sql 연결
+            Connection connection = DriverManager.getConnection(url, user, password);
+            // statement : edit 생성
+            Statement statement = connection.createStatement();
+
+            // query 및 resultSet 변수 미리 생성
+            String query;
+            String query2;
+            ResultSet resultSet;
+            ResultSet resultSet2;
+
+            // Scanner 인스턴스 및 Scanner 변수 : in 설정.
+            Scanner in = new Scanner(System.in);
+            String key = ""; // key 에 변수 초기화
+            System.out.println("------ 작동 key ------");
+            System.out.println("(E)xit : 종료");
+            System.out.println("(P)oll : 설문 시작");
+            System.out.println("(S)tatistic : 설문 통계");
+            while (!(key.equals("E") || key.equals("Exit"))) {
+                System.out.println("선택 입력 : ");
+                key = in.nextLine(); // key 입력 받기
+                // P 랑 Poll 입력시 안에 if 문 실행
+                if (key.equals("P") || key.equals("Poll")) {
+
+                }
+                // S 랑 Statistic 입력시 else if 문 실행
+                else if (key.equals("S") || key.equals("Statistic")) {
+
+                }
+                // 그 외 입력 값에 대한 처리.
+                else {
+                    System.out.println("잘못 입력. 입력값 : " + key);
+                }
+            }
+            System.out.println("----- 설문 종료 ------");
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            
+        }
+    }
 }
