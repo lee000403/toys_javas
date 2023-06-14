e95a0144eac4f3f0f52a0ef817273d2c99d1d405
lee000403 <dong000403@gmail.com>
2023-06-14 13:11:18 +0900

    이동환 수정

--- src/PollsWithDBInTerminal/terminalpolls/PollList.java
+++ src/PollsWithDBInTerminal/terminalpolls/PollList.java
@@ -15,31 +15,25 @@ public class PollList {
             ResultSet resultSet2;
        
             // 문제 받는 쿼리
-            query = "SELECT QUESTION\n" + //
-                    "FROM questions\n" + //
-                    ";";
+            query = "SELECT QUESTION, ANSWER\n" + //
+                    "FROM answers  AS T_ANSWE\n" + //
+                    "JOIN questions AS T_QUEST\n" + //
+                    "ORDER BY QUESTION, ANSWER";
             resultSet = statement.executeQuery(query);
 
-            // 답항 뽑는 쿼리
-            query2 = " SELECT ANSWER\n" + //
-                    "FROM answers;";
-
+            int num =1;
             // 질문과 답항을 출력
             while (resultSet.next()) {
-                System.out.println(resultSet.getString("QUESTION"));
-
-                resultSet2 = statement.executeQuery(query2);
-                while (resultSet2.next()) {
-                    
-                    System.out.print(resultSet2.getString("ANSWER"));
+                if (num%4 == 1) {
+                    System.out.println(resultSet.getString("QUESTION"));
                 }
-                System.out.println(); //줄바꿈
-                System.out.print("답 : " );
-                int first = myObj.nextInt(); 
-                System.out.println( first);
-
-                
-
+                System.out.print(resultSet.getString("ANSWER"));
+                if (num%4 == 0) {
+                    System.out.println();
+                    System.out.print("답 : " );
+                    int first = myObj.nextInt();
+                }
+                num++;
                 
             }
 
