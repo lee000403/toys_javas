5644f3961e14902cf608ff8fa5f771fbdf1c5c78
GabaeOh <rkqo4577@gmail.com>
2023-06-14 16:41:02 +0900

    rkqo

---
+++ src/PollsWithDBInTerminal/terminalpolls/Commons.java
@@ -0,0 +1,9 @@
+package PollsWithDBInTerminal.terminalpolls;
+
+import java.util.UUID;
+
+public class Commons {
+     public String generateUUID() {
+        return UUID.randomUUID().toString();
+    }
+}
