41cabcd86b877e297a3c23d177ed1f92432df42f
lee000403 <dong000403@gmail.com>
2023-06-14 16:35:54 +0900

    이동환 수정

--- src/PollsWithDBInTerminal/PollsWithDB.java
+++ src/PollsWithDBInTerminal/PollsWithDB.java
@@ -43,12 +43,12 @@ public class PollsWithDB {
             while (!(key.equals("E") || key.equals("Exit"))) {
                 System.out.print("선택 입력 : ");
                 key = in.nextLine(); // key 입력 받기
-                int users = pollPeople.users(statement);
+                String users = pollPeople.users(statement);
 
                 // P 랑 Poll 입력시 안에 if 문 실행
                 if (key.equals("P") || key.equals("Poll")) {
                     System.out.println();
-                    String answers = pollList.pollList(statement, connection);
+                    String answers = pollList.pollList(statement, connection, users);
                 }
 
                 // S 랑 Statistic 입력시 else if 문 실행
--- src/PollsWithDBInTerminal/terminalpolls/PollPeople.java
+++ src/PollsWithDBInTerminal/terminalpolls/PollPeople.java
@@ -1,62 +1,50 @@
 package PollsWithDBInTerminal.terminalpolls;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Scanner;
 import java.util.HashMap;
 
 public class PollPeople {
-    public int users(Statement statement) throws SQLException {
+    public String users(Statement statement) throws SQLException {
+        Scanner in = new Scanner(System.in);
+        // 설문자 입력 쿼리
+        String query = "SELECT USER_ID, USER\n" +
+                "FROM users AS T_USER ;\n";
 
-        try {
-            // 설문자 입력 쿼리
-            String query = "SELECT USER_ID, USER\n" +
-                    "FROM users AS T_USER ;\n";
+        ResultSet resultSet = statement.executeQuery(query);
 
-            ResultSet resultSet = statement.executeQuery(query);
+        System.out.println("- 설문자 가능 명단(가입 완료)");
 
-            System.out.println("- 설문자 가능 명단(가입 완료)");
+        // 설문자 number 변수 초기화 후 설문 참여자 번호 표시
+        int number = 1; // 설문 참여자 번호를 나타내는 변수 초기값 1로 설정
+        String user_id;
+        int user_number;
+        HashMap<Integer, String> userNumberMap = new HashMap<>();
 
-            // 설문자 number 변수 초기화 후 설문 참여자 번호 표시
-            int number = 1; // 설문 참여자 번호를 나타내는 변수 초기값 1로 설정
-
-            HashMap<Integer, String> userNumberMap = new HashMap<>();
-
-
-
-            // 현재 행에서 "USER_NAME" 컬럼의 값을 도출 설문 참여자 이름
-            while (resultSet.next()) {
-                String userId = resultSet.getString("USER_ID");
-                String userName = resultSet.getString("USER");
-                // 설문참여자의 번호 (number)와 이름 (userName) 출력
-                System.out.println(number + ". " + userName);
-                // 설문참여자의 번호를 1씩 증가
-                userNumberMap.put(number, userName);
-                number++;
-            }
-
-            // 설문자 번호 입력 메시지 출력
+        // 현재 행에서 "USER_NAME" 컬럼의 값을 도출 설문 참여자 이름
+        while (resultSet.next()) {
+            String userId = resultSet.getString("USER_ID");
+            String userName = resultSet.getString("USER");
+            // 설문참여자의 번호 (number)와 이름 (userName) 출력
+            System.out.print(number + ". " + userName);
+            // 설문참여자의 번호를 1씩 증가
+            userNumberMap.put(number, userId);
+            number++;
+        }
+        while (true) {
             System.out.print("- 설문자 번호 입력 : ");
-            Scanner in = new Scanner(System.in);
-            // 메소드 사용 후 데이터 입력 받음
-            int userId = in.nextInt();
-
-            // 선택된 설문자의 번호와 이름 출력
-            while ( userId<= 0 && userId <= 4) {
-                System.out.println("- Error - 확인 후 입력 필요");
-                System.out.print("- 설문자 번호 입력 : ");
-                number = in.nextInt();
+            user_number = in.nextInt();
+            if (user_number > 4) {
+                System.out.println("-Error- 확인후 입력 필요");
             }
-
-            System.out.println("-- 설문 시작");
-            System.out.println("\t--> 아래 참조 : poll contents example");
-
-        } catch (SQLException e) {
-            System.out.println("SQL Exception: " + e.getMessage());
-
-        }   
-        return 0;
+            else{
+                break;
+            }
+        }
+        user_id = userNumberMap.get(user_number);
+        System.out.println(user_id);
+        System.out.println("-- 설문 시작");
+        System.out.println("\t--> 아래 참조 : poll contents example");
+        return user_id;
     }
 }
