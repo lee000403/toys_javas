a0f16e010a9e85b67ab1c8cd8a6898729016362a
lee000403 <dong000403@gmail.com>
2023-06-14 15:38:41 +0900

    이동환

--- src/PollsWithDBInTerminal/terminalpolls/PollList.java
+++ src/PollsWithDBInTerminal/terminalpolls/PollList.java
@@ -9,41 +9,41 @@ import java.util.Scanner;
 
 public class PollList {
     public String pollList(Statement statement, Connection connection) throws SQLException {
-            Scanner in = new Scanner(System.in);
-            String query;
-            ResultSet resultSet;
-            String query2;
-            ResultSet resultSet2;
-            // 문제 받는 쿼리
-            query = "SELECT QUESTION, QUESTION_ID\n" + //
-                    "FROM questions AS T_QUEST";
-            resultSet = statement.executeQuery(query);
-            
-            HashMap <String, String>ans_ans = new HashMap<String, String>();
-            HashMap <String, String>que_ans = new HashMap<String, String>();
-            
-            int number = 1;
-            // 질문과 답항을 출력
-            while (resultSet.next()) {
-                statement = connection.createStatement();
-                System.out.println(resultSet.getString("QUESTION"));
-                
-                query2 = "SELECT ANSWER, ANSWER_ID\n" + //
+        Scanner in = new Scanner(System.in);
+        String query;
+        ResultSet resultSet;
+        String query2;
+        ResultSet resultSet2;
+        // 문제 받는 쿼리
+        query = "SELECT QUESTION, QUESTION_ID\n" + //
+                "FROM questions AS T_QUEST";
+        resultSet = statement.executeQuery(query);
+
+        HashMap<String, String> ans_ans = new HashMap<String, String>();
+        HashMap<String, String> que_ans = new HashMap<String, String>();
+
+        // 질문과 답항을 출력
+        while (resultSet.next()) {
+            System.out.println();
+            statement = connection.createStatement();
+            System.out.println(resultSet.getString("QUESTION"));
+
+            query2 = "SELECT ANSWER, ANSWER_ID\n" + //
                     "FROM answers AS T_ANSWE";
-                resultSet2 = statement.executeQuery(query2);
-                statement = connection.createStatement();
-                while (resultSet2.next()) {
-                    System.out.print(resultSet2.getString("ANSWER"));
-                }
-                System.out.println();
-                System.out.print("답 입력 : ");
-                int answer = in.nextInt();
-                que_ans.put(resultSet.getString("ANSWER_ID"), String.valueOf(number));
-                ans_ans.put(resultSet.getString("QUESTION_ID"), String.valueOf(answer));
-                System.out.println(ans_ans);
+            resultSet2 = statement.executeQuery(query2);
+            statement = connection.createStatement();
+            int number = 1;
+            while (resultSet2.next()) {
+                System.out.print(resultSet2.getString("ANSWER"));
+                ans_ans.put(String.valueOf(number), resultSet2.getString("ANSWER_ID"));
                 number++;
-                
-                }
-            return null;
+            }
+            System.out.println();
+            System.out.print("답 입력 : ");
+            int answer = in.nextInt();
+            que_ans.put(resultSet.getString("QUESTION_ID"), ans_ans.get(String.valueOf(answer)));
+        }
+        System.out.println(que_ans);
+        return null;
     }
 }
