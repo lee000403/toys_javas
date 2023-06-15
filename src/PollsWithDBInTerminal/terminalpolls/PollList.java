5591a647845a35a43f2890691a1b8bcad1ca642f
lee000403 <dong000403@gmail.com>
2023-06-15 12:12:35 +0900

    주석 다 달았어요.

--- src/PollsWithDBInTerminal/terminalpolls/PollList.java
+++ src/PollsWithDBInTerminal/terminalpolls/PollList.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 
 public class PollList {
     public String pollList(Statement statement, Connection connection, String user_id) throws SQLException {
+        // Scanner 인스턴스화 변수 : in
         Scanner in = new Scanner(System.in);
         String query;
         ResultSet resultSet;
@@ -19,15 +20,18 @@ public class PollList {
                 "FROM questions AS T_QUEST";
         resultSet = statement.executeQuery(query);
 
+        // HashMap 생성
         HashMap<String, String> ans_ans = new HashMap<String, String>();
-        HashMap<String, String> que_ans = new HashMap<String, String>();
+
         int answer;
-        // 질문과 답항을 출력
+        // DDL 삭제 부분
         Statement statement2 = connection.createStatement();
         query = "DELETE \n" + //
                 "FROM statistics\n" +
                 "WHERE USER_ID = '" + user_id + "'";
+        // count3 : debuging 확인용
         int count3 = statement2.executeUpdate(query);
+        // 문항 출력 LOOP 
         while (resultSet.next()) {
             System.out.println();
             statement = connection.createStatement();
@@ -38,7 +42,7 @@ public class PollList {
             resultSet2 = statement.executeQuery(query2);
             statement = connection.createStatement();
             int number = 1;
-
+            // 답항 출력 LOOP
             while (resultSet2.next()) {
                 System.out.print(resultSet2.getString("ANSWER"));
                 ans_ans.put(String.valueOf(number), resultSet2.getString("ANSWER_ID"));
@@ -47,12 +51,15 @@ public class PollList {
             System.out.println();
             System.out.print("답 입력 : ");
             answer = in.nextInt();
+            // 답항 개수 제한
             if (answer > 4) {
                 System.out.println("잘못 입력 하였습니다. \n" + "처음으로 돌아 갑니다. ");
                 break;
             }
+            // UUID 인스턴스화 및 호출
             Commons commons = new Commons();
             String statPk = commons.generateUUID();
+            // DATEBASE 에 추가할 DDL 문
             query = "INSERT INTO statistics\n" + //
                     "(USER_ID, QUESTION_ID, ANSWER_ID, STATISTICS_ID)\n" + //
                     "value\n" + //
